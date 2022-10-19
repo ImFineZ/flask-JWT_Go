7f6a4c4f0f375e042696386da0a1c3d840860152
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-10-19 04:09:58 +0000

    primera parte

--- src/app.py
+++ src/app.py
@@ -71,6 +71,7 @@ def create_token():
     email = request.json.get('email', None)
     password = request.json.get('password', None)
     if email != "test" or password != "test":
+        print(email,password)
         return jsonify({"msg":"Wrong email or password"}), 401
 
     acces_token = create_access_token(identity=email)
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,34 +1,30 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
-  const { email, setEmail } = useState("");
-  const { password, setPassword } = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleClick = () => {
     const opts = {
       method: "POST",
       headers: {
-        "Content-type": "application/json",
+        "Content-type": "application/json"
       },
       body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
+        "email": email,
+        "password": password
+      })
     };
-    fetch(
-      "https://3001-gonza5150-flaskjwt-sntdmlw1imh.ws-us71.gitpod.io/token",
-      opts
-    )
-      .then((resp) => {
+    fetch("https://3001-gonza5150-flaskjwt-sntdmlw1imh.ws-us71.gitpod.io/token", opts)
+      .then(resp => {
         if (resp.status === 200) return resp.json();
-        else alert("Sorry, it was an error!!!");
+        else alert("Sorry, it was an error!!!")
       })
       .then()
-      .catch((error) => {
+      .catch(error => {
         console.error("Sorry, it was an error!!!", error);
       });
   };
@@ -37,18 +33,8 @@ export const Login = () => {
     <div className="text-center mt-5">
       <h1>Login</h1>
       <div>
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button onClick={handleClick}>Login</button>
       </div>
     </div>
