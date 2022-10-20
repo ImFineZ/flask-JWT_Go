ef05e4d0fbb45126b3c080b85f7d352311feae9a
ImFineZ <greivinrodriguez@icloud.com>
2022-10-20 03:02:54 +0000

    finalizad

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <p>
       Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="https://github.com/Gonza-5150/flask-JWT">Gonzalo Esteves</a>
+      <a href="https://github.com/ImFineZ">Greivin Rodriguez</a>
     </p>
   </footer>
 );
